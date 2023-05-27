e844e1f77027777dd4aedc89658e82c668e64c70
SandeshGC <gcsandesh01@gmail.com>
2023-05-27 16:44:07 +0545

    create 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Layout from "./containers/Layout/Layout"
+import { Home, Page404 } from "./pages"
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Layout />}>
-					<Route index element={<h1>Home</h1>} />
-					<Route path="*" element={<h1>404</h1>} />
+					<Route index element={<Home />} />
+					<Route path="*" element={<Page404 />} />
 				</Route>
 			</Routes>
 		</Router>
---
+++ src/pages/Page404.tsx
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography"
+
+const Page404 = () => {
+	return (
+		<>
+			<Typography variant={"h1"} fontWeight={800} textAlign={"center"}>
+				404
+			</Typography>
+			<Typography align="center">Page not found!</Typography>
+		</>
+	)
+}
+
+export default Page404
