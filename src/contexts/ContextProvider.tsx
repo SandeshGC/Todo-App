2c4dac355c6b1a88eab13c50cf4a126a24baa1a9
SandeshGC <gcsandesh01@gmail.com>
2023-10-26 22:05:15 +0545

    ui: modify theme for font family

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,8 +1,14 @@
 import { ReactNode } from 'react'
 import ModalProvider from './ModalContext'
+import { ThemeProvider } from '@mui/material'
+import theme from '../utils/theme'
 
 const ContextProvider = ({ children }: { children: ReactNode }) => {
-	return <ModalProvider>{children}</ModalProvider>
+	return (
+		<ThemeProvider theme={theme}>
+			<ModalProvider>{children}</ModalProvider>
+		</ThemeProvider>
+	)
 }
 
 export default ContextProvider
